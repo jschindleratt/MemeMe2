281ec9bf6d5ff2f528ff687c6340f8055530437c
Josh Schindler <js2387@att.com>
2017-07-30 16:57:52 -0400

    first commit for review

---
+++ LearnPicturePicker/BottomTextDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  BottomTextDelegate.swift
+//  LearnPicturePicker
+//
+//  Created by Joshua Schindler on 7/12/17.
+//  Copyright © 2017 Joshua Schindler. All rights reserved.
+//
+
+import Foundation
---
+++ LearnPicturePicker/TopTextDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  TopTextDelegate.swift
+//  LearnPicturePicker
+//
+//  Created by Joshua Schindler on 7/12/17.
+//  Copyright © 2017 Joshua Schindler. All rights reserved.
+//
+
+import Foundation
